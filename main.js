efab8e6fefccc3970b9367dabadf3cb30b85e725
samvanrosmalen <samvanrosmalen@live.nl>
2021-03-02 11:21:06 +0100

    random keuze computer maken

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const game = ()=>{
+    //Beginwaarde scoreboard
     let pScore = 0;
     let cScore = 0;
-
+    
+    //Start het spel
     const startGame = () => {
         const playBtn = document.querySelector(".intro button");
         const introScreen = document.querySelector(".intro");
@@ -9,9 +11,21 @@ const game = ()=>{
 
         playBtn.addEventListener("click", () => { 
             introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
         });
     };
+    //play match
+    const playMatch = () => {
+        const options = document.querySelectorAll ('.options button');
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
 
+        //computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        const computerNumber = Math.floor(Math.random()*3);
+        console.log(compterNumber);
+    }
     //Is call all the inner function
     startGame();
     
