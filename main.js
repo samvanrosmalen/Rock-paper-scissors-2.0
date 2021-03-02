f92db90850dd5bb658a5ad35801add6957508a8f
samvanrosmalen <samvanrosmalen@live.nl>
2021-03-02 11:13:18 +0100

    transisiton toegevoegd

--- main.js
+++ main.js
@@ -2,5 +2,20 @@ const game = ()=>{
     let pScore = 0;
     let cScore = 0;
 
+    const startGame = () => {
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
+
+        playBtn.addEventListener("click", () => { 
+            introScreen.classList.add("fadeOut");
+        });
+    };
+
+    //Is call all the inner function
+    startGame();
     
-}
+};
+
+//start game function
+game();
